e129e462b2cbbf33bfd1482900cc95d96c208c94
mayberedfy <xujianyi41@163.com>
2025-07-30 14:45:39 +0800

    change app run host to localhost

--- src/app.py
+++ src/app.py
@@ -23,4 +23,4 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # Ensure all tables are created
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
